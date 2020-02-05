c3ded09510a3d723b7bc7ec7d0b68edca71c070f
z025424 <z025424@INLH104100.corp.noxiane.net>
2020-02-05 12:20:32 +0530

    Initial commit

---
+++ 5_SpringBoot_Devtools/src/main/java/com/springboot/controller/GreetingController.java
@@ -0,0 +1,14 @@
+package com.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	@RequestMapping("/")
+	public @ResponseBody String greeting() {
+		return "welcome";
+	}
+}
